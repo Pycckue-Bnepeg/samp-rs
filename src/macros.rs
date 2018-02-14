eef27462e23a70adee14bf42c26a84ad1cc04efb
ZOTTCE <zottce@gmail.com>
2018-02-14 13:44:07 +0300

    natives! example

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,15 @@
 #[macro_export]
 /// `natives!` macro
+///
+/// # Examples
+///
+/// ```
+/// let natives = natives![
+///    { "ShowSomething", show_something },
+///    { "WhereIsPlayer", where_is_player }
+/// ];
+/// amx.register(natives);
+/// ```
 macro_rules! natives {
     [ $( { $name:expr, $func:ident } ),* ] => {
         {
