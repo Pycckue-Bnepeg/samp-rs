b6c2c190412f70ba7c9cd63d32222d447dc16818
ZOTTCE <zottce@gmail.com>
2018-10-05 15:44:40 +0300

    add cp1251::encode_to and cp1251::decode_to

--- src/cp1251.rs
+++ src/cp1251.rs
@@ -10,3 +10,11 @@ pub fn encode(string: &str) -> AmxResult<Vec<u8>> {
 pub fn decode(bytes: &[u8]) -> AmxResult<String> {
     WINDOWS_1251.decode(bytes, DecoderTrap::Strict).map_err(|_| AmxError::Format)
 }
+
+pub fn encode_to(source: &str, dest: &mut Vec<u8>) -> AmxResult<()> {
+    WINDOWS_1251.encode_to(source, EncoderTrap::Strict, dest).map_err(|_| AmxError::Format)
+}
+
+pub fn decode_to(source: &[u8], dest: &mut String) -> AmxResult<()> {
+    WINDOWS_1251.decode_to(source, DecoderTrap::Strict, dest).map_err(|_| AmxError::Format)
+}
