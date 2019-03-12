93ccf48f5cab155f81d9330ce79dc4f8697bc110
ZOTTCE <zottce@gmail.com>
2019-03-12 12:19:37 +0300

    logger examples

--- plugin-example/src/lib.rs
+++ plugin-example/src/lib.rs
@@ -4,7 +4,7 @@ use samp::error::AmxResult;
 use samp::plugin::SampPlugin;
 use samp::{initialize_plugin, native};
 
-use log::{info, error};
+use log::{info, error, debug};
 
 use memcache::Client;
 
@@ -141,6 +141,7 @@ impl SampPlugin for Memcached {
     fn on_load(&mut self) {
         info!("that's a info msg");
         error!("that's an error msg");
+        debug!("testique");
     }
 }
 
@@ -158,11 +159,27 @@ initialize_plugin!(
         samp::plugin::enable_process_tick();
         samp::encoding::set_default_encoding(samp::encoding::WINDOWS_1251); // Cyrillic
 
-        let logger = samp::plugin::logger();
+        // get a default samp logger (uses samp logprintf).
+        let samp_logger = samp::plugin::logger()
+            .level(log::LevelFilter::Info); // logging only info, warn and error messages
 
-        let _ = logger.format(|callback, message, record| {
-            callback.finish(format_args!("memcached {}: {}", record.level().to_string().to_lowercase(), message))
-        }).apply();
+        let log_file = fern::log_file("myplugin.log").expect("Something wrong!");
+
+        // log trace and debug messages in an another file
+        let trace_level = fern::Dispatch::new()
+            .level(log::LevelFilter::Trace)
+            .chain(log_file);
+
+        let _ = fern::Dispatch::new()
+            .format(|callback, message, record| {
+                // all messages will be formated like
+                // memcached error: something (error!("something"))
+                // memcached info: some info (info!("some info"))
+                callback.finish(format_args!("memcached {}: {}", record.level().to_string().to_lowercase(), message))
+            })
+            .chain(samp_logger)
+            .chain(trace_level)
+            .apply();
         
         return Memcached {
             clients: Vec::new(),
--- samp/src/plugin.rs
+++ samp/src/plugin.rs
@@ -40,6 +40,49 @@ pub fn enable_process_tick() {
     runtime.enable_process_tick();
 }
 
+/// Get a fern [`Dispatch`] and disable auto installing logger.
+/// 
+/// # Example
+/// ```rust,compile_fail
+/// use samp::initialize_plugin;
+/// use samp::prelude::*;
+/// 
+/// use std::fs::OpenOptions;
+///
+/// struct MyPlugin;
+///
+/// impl SampPlugin for MyPlugin {}
+///
+/// initialize_plugin!({
+///     samp::plugin::enable_process_tick();
+///     
+///     // get a default samp logger (uses samp logprintf).
+///     let samp_logger = samp::plugin::logger()
+///         .level(log::LevelFilter::Warn); // logging only warn and error messages
+///
+///     let log_file = fern::log_file("myplugin.log").expect("Something wrong!");
+///
+///     // log trace and debug messages in an another file
+///     let trace_level = fern::Dispatch::new()
+///         .level(log::LevelFilter::Trace) // write ALL types of logs
+///         .chain(log_file);
+///
+///     let _ = fern::Dispatch::new()
+///         .format(|callback, message, record| {
+///             // all messages will be formated like
+///             // [MyPlugin][ERROR]: something (error!("something"))
+///             // [MyPlugin][INFO]: some info (info!("some info"))
+///             callback.finish(format_args!("[MyPlugin][{}]: {}", record.level(), message))
+///         })
+///         .chain(samp_logger)
+///         .chain(trace_level)
+///         .apply();
+/// 
+///     return MyPlugin;
+/// });
+/// ```
+/// 
+/// [`Dispatch`]: https://docs.rs/fern/0.5.7/fern/struct.Dispatch.html
 pub fn logger() -> fern::Dispatch {
     let rt = Runtime::get();
     rt.disable_default_logger();
